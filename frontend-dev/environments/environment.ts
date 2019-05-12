578f2283d852b6a0f9a2225d3c47766b9978466e
kyota <kyotawatanabe@KyotanoMacBook.local>
2019-05-12 14:58:01 +0900

    add demoode

--- frontend-dev/environments/environment.ts
+++ frontend-dev/environments/environment.ts
@@ -6,6 +6,7 @@
 export const environment = {
     production: false,
     APP_APISERVER_URL: 'http://localhost:6171/',
-    TRADE_APISERVER_URL: 'http://127.0.0.1:8000/',
-    environment: 'dev'
+    TRADE_APISERVER_URL: 'http://localhost:8000/',
+    environment: 'dev',
+    demo_mode: true,
 };
